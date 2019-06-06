2b291748c60097a3b4c78e60084163f97a11fec9
Majed Amin <majedamin974@gmail.com>
2019-06-06 09:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + katzDeliLine.shift() + ".";
   }
 }
+
+
+function currentLine(katzDeliLine) {
+  var newArr = [];
+
+  if (katzDeliLine.length > 0) {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      newArr.push((i + 1) + ". " katzDeliLine[i]);
+    }
+    return "The line is currently:" + newArr;
+  } else {
+    return "The is currently empty.";
+  }
+}
